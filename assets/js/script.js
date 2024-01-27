3378163cca32ebdc9b4bb84d3173b4eff0f21763
Sarolta <sarolta.hegyi@gmail.com>
2024-01-27 14:01:12 +0100

    add api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-Sd2srdZlZp-GXYC2Ud8AYTFfZJY
+const API_KEY="Sd2srdZlZp-GXYC2Ud8AYTFfZJY"
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultModal = new bootstrap.Modal(document.getElementById("resultModal"));
+
+document.getElementById("status").addEventListener("click", e =>getStatus(e));
+
+async function getStatus(e) {
+  const quesryString = `${API_URL}?api_key=${API_KEY}`;
+  
+  const response = await fetch(queryString);
+  const data = await response.json();
+
+  if (response.ok) {
+    console.log(data);
+  }
+}
