98af49ab7db2bd3d734a62df204ca6534a52d30f
Sarolta <sarolta.hegyi@gmail.com>
2024-01-29 17:49:52 +0100

    add postForm

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,19 @@ const resultModal = new bootstrap.Modal(document.getElementById("resultModal"));
 document
   .getElementById("status")
   .addEventListener("click", (e) => getStatus(e));
+document.getElementById("submit").addEventListener("click", (e) => postForm(e));
+
+async function postForm(e) {
+  const form = new FormData(document.getElementById("checksform"));
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      Authorization: API_KEY,
+    },
+    body: form,
+  });
+}
 
 async function getStatus(e) {
   const quesryString = `${API_URL}?api_key=${API_KEY}`;
