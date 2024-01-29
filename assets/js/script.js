8b383417e01010862b6fbb36929aac3333955cd6
Sarolta <sarolta.hegyi@gmail.com>
2024-01-29 17:33:34 +0100

    add error handling else

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ async function getStatus(e) {
   const data = await response.json();
 
   if (response.ok) {
-    console.log(data);
+    console.log(data.expiry);
+  } else {
+    throw new Error (data.error):
   }
 }
